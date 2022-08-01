497ce49d862662172f23e2c8ded533c13326350b
Flaviane Ribeiro <rnany13@gmail.com>
2022-08-01 14:26:17 -0300

    refactor: Ajuste no código

--- back-end/index.js
+++ back-end/index.js
@@ -5,15 +5,12 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 app.use(cors());
 app.use(bodyparser.json());
 
-
-// Rotas
 const routerCliente = require('./router/index')
 app.use(routerCliente);
 
-app.listen(3000, () =>{
+app.listen( process.env.PORT || 3000, () =>{
     console.log('Servidor rodando')
 })
--- back-end/router/index.js
+++ back-end/router/index.js
@@ -1,15 +1,3 @@
-/* const  express = require('express');
-const router = express.Router();
-
-router.get('/client', clientController.getclient)
-router.get('/clientAll', clientController.getProdutAll)
-router.get('/client/:id', clientController.getProdutId)
-router.post('/client', clientController.creat)
-router.delete('/client/:id', clientController.delete)
-router.put('/client/:id', clientController.update)
-
-*/
-
 const  express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/client-controllers')
